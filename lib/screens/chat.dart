339f9da1eb89aa7a9a4b0a7a244a5e26a5a84325
pasheza <pashezik@gmail.com>
2022-02-20 13:36:21 +0300

    Implemented list of messages

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:surf_practice_chat_flutter/data/chat/repository/repository.dart';
-
-/// Chat screen templete. This is your starting point.
+import 'package:surf_practice_chat_flutter/data/chat/chat.dart';
+/// Chat screen template. This is your starting point.
 class ChatScreen extends StatefulWidget {
   final ChatRepository chatRepository;
 
@@ -33,14 +32,49 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
-      body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            return Card();
+
+      body: FutureBuilder<List<ChatMessageDto>>(
+        future: widget.chatRepository.messages,
+        builder: (BuildContext context, AsyncSnapshot<List<ChatMessageDto>> snapshot) {
+          Widget child;
+          if (snapshot.hasData) {
+            child = ListView.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (_,index) => MessageCard(message: snapshot.data![index]) // Fix null
+            );
+          } else if (snapshot.hasError) {
+              child = Column(
+                children: [
+                  const Icon(
+                    Icons.error_outline,
+                    color: Colors.red,
+                    size: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Text('Error: ${snapshot.error}'),
+                  )
+                ],
+              );
+          } else {
+            child = Column(
+              children: const [
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text('Awaiting result...'),
+                )
+              ],
+            );
           }
+          return child;
+        },
       ),
-      
-      
-      
+
       bottomNavigationBar: BottomAppBar(
         child: Row(
           children: [
@@ -57,6 +91,27 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MessageCard extends StatelessWidget {
+
+  final ChatMessageDto message;
+
+  const MessageCard({Key? key, required this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Theme.of(context).primaryColor,
+          child: Text(message.author.name[0]),
+        ),
+        title: Text(message.author.name),
+        subtitle: Text(message.message),
       ),
     );
   }
